6ca1d6380514da9b3f9ad409862fe18c5ad0a6b3
TBibby68 <j.smith321456798@gmail.com>
2025-08-19 18:27:12 +0100

    setting up the paper trading file to initialise with the last 3 months of data from the table for now.

--- PCAPostGresTableGeneration.py
+++ PCAPostGresTableGeneration.py
@@ -1,6 +1,33 @@
 import pandas as pd
+import psycopg2
+import configparser
 
 # this file just creates a new postgres table designed to fit the PCA backtesting data in - 100 stocks 
 
-# 1) import tickers from excel using pandas:
-df = pd.read_excel("banks to trade on.xlsx")
+def GeneratePCATable():
+    # 1) import tickers from excel using pandas:
+    stock_tickers = pd.read_excel("banks to trade on.xlsx")
+    tickers = stock_tickers.iloc[:, 0].dropna().unique().tolist()
+
+    # 2) define SQL query: 
+    columns_sql = ", ".join([f'"{ticker}" FLOAT' for ticker in tickers])
+    create_table_sql = f"CREATE TABLE hundred_stock_prices ({columns_sql});"
+
+    # 3) run on my postgres database:
+    config = configparser.ConfigParser()
+    config.read('db_config.ini')
+
+    params = config['postgresql']
+
+    # Use unpacking to connect
+    conn = psycopg2.connect(**params)
+    
+    cur = conn.cursor()
+    cur.execute(create_table_sql)
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+# run the function
+GeneratePCATable()
--- paperStream.py
+++ paperStream.py
@@ -6,10 +6,24 @@ import numpy as np
 from signals import update_and_get_signal # to get the trading signals
 from trading import place_pair_trade # to actually do the trading
 import GlobalVariables
+from sqlalchemy import create_engine # for the 3 months to jump start it
+from config import engine_string
+import pandas as pd
 
 # NOTE: keep this file for normal stocks only, and then we can have a separate file for crypto
 
 # NOTE: THIS WILL NOT WORK WITH WEBSOCKETS 15+, SO PUT THIS IN A DIFFERENT ENVIRONMENT THAN THE YFINANCE BACKTEST
+
+engine = create_engine(engine_string)
+# literally just query the db. 
+def find_initial_pair(engine):
+    df = pd.read_sql('SELECT * FROM cointegration_results WHERE window_id = 0 LIMIT 1', con=engine)
+    pair = []
+    pair.append(df['stock1'])
+    pair.append(df['stock2'])
+    # these will be treated as series, even though we know they only have one value, so if we try to print them they will 
+    # print the meta data as well, like the name, type and length
+    return pair
 
 # method to compute the analytical solution for the hedge ratio from a linear regression between the two stocks. 
 # we will use around 200 minute rolling history for this beta as we trade around every 30 minutes. 
@@ -21,12 +35,15 @@ def compute_beta(aapl_prices, msft_prices):
 # This file handles the streaming of prices from the alpaca API, and then calls the signals file and the trading file to actually
 # perform the trades and calculate when the spread has widened enough to enter a position
 
-aapl_price = None
+stock1_price = None
+initial_pair = find_initial_pair(engine)
+stock1_ticker = initial_pair[0].iloc[0]
+stock2_ticker = initial_pair[1].iloc[0]
 
 # setting up the websocket connection directly here 
 async def alpaca_socket():
-    global aapl_price  # so we can update it
-    global msft_price
+    global stock1_price  # so we can update it
+    global stock2_price
     try:
         async with websockets.connect(stream_url) as ws:
             # Authenticate with the usual details
@@ -40,7 +57,7 @@ async def alpaca_socket():
             # Subscribe to bars: the exchange rate from USD to GBP will effect this a little.
             sub_msg = {
                 "action": "subscribe",
-                "bars": ["AAPL", "MSFT"]
+                "bars": [stock1_ticker, stock2_ticker]
             }
             # we get json responses from the API. 
             await ws.send(json.dumps(sub_msg)) # this pulls the response 
@@ -55,19 +72,19 @@ async def alpaca_socket():
                 if isinstance(data, list):
                     for item in data:
                         if item.get("T") == "b": # an OHLCV bar message: this is the response we need
-                            if item.get("S") == 'AAPL':
-                                aapl_price = item["c"]  # close price of apple
-                                print("this is appl price " + str(aapl_price))
-                            elif item.get("S") == 'MSFT':
-                                msft_price = item["c"] # close price of microsoft
-                                print("this is msft price " + str(msft_price))
+                            if item.get("S") == stock1_ticker:
+                                stock1_price = item["c"]  # close price of stock 1 
+                                print(f"this is {stock1_ticker} price " + str(stock1_price))
+                            elif item.get("S") == stock2_ticker:
+                                stock2_price = item["c"] # close price of stock 2
+                                print(f"this is {stock2_ticker} price " + str(stock2_price))
                 
-                            if msft_price is not None and aapl_price is not None:
+                            if stock2_price is not None and stock1_price is not None:
                                 #beta = compute_beta(aapl_prices, msft_prices)
-                                signal = update_and_get_signal(aapl_price, msft_price, 1) # want this to return the z scores queue as well
+                                signal = update_and_get_signal(stock1_price, stock2_price, 1) # want this to return the z scores queue as well
                                 if signal not in (None, GlobalVariables.last_signal):
                                     # need to pull through the current and previous z scores: current is -1 and previous is 0
-                                    place_pair_trade("AAPL", "MSFT", 10, GlobalVariables.z_scores[-1], GlobalVariables.z_scores[0], signal)
+                                    place_pair_trade(stock1_ticker, stock2_ticker, 10, GlobalVariables.z_scores[-1], GlobalVariables.z_scores[0], signal)
                                     GlobalVariables.last_signal = signal
                 else:
                     print("Non-bar message:", data)
