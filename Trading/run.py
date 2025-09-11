a8b6e022f86cc73c726154eaf472676354bd3613
TBibby68 <j.smith321456798@gmail.com>
2025-09-11 21:47:08 +0100

    .

--- Trading/run.py
+++ Trading/run.py
@@ -3,7 +3,7 @@ import StatArbBot.Trading.starterForCloudInstanceOfBot
 import StatArbBot.Trading.paperStream
 
 def main():
-    print("Running file1...")
+    print("booting up stat arb bot...")
     StatArbBot.Trading.pricesStarter.main()
     StatArbBot.Trading.starterForCloudInstanceOfBot.main()
     StatArbBot.Trading.paperStream.main()
