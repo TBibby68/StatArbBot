7f3860687f518ce43bdf49e948659f856ea679ca
TBibby68 <j.smith321456798@gmail.com>
2025-09-15 20:18:31 +0100

    .

--- Trading/run.py
+++ Trading/run.py
@@ -1,12 +1,12 @@
 import StatArbBot.Trading.pricesStarter
 import StatArbBot.Trading.starterForCloudInstanceOfBot
-import Trading.alpacaStream
+import StatArbBot.Trading.ibkrAPIEvents
 
 def main():
     print("booting up stat arb bot...")
     StatArbBot.Trading.pricesStarter.main()
     StatArbBot.Trading.starterForCloudInstanceOfBot.main()
-    StatArbBot.Trading.alpacaStream.main()
+    StatArbBot.Trading.ibkrAPIEvents.main()
 
 if __name__ == "__main__":
     main()
