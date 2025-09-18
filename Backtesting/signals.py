c526bb197bddff461671a7865fa8bcd726ec0aeb
TBibby68 <j.smith321456798@gmail.com>
2025-09-18 14:08:51 +0100

    temporaily removing the position sizing factor so I can just testing the paper trading module with the simplest model possible

--- Backtesting/signals.py
+++ Backtesting/signals.py
@@ -29,13 +29,13 @@ def update_and_get_signal(price_a, price_b, beta=1.0):
     # Example signal logic: definitely needs some work: it's currently just entering and has no exit logic essentially
     if abs(z) > 1.5 and GlobalVariables.last_signal != "OPEN":
         # the further we get away from the threshold the riskier the signal is so we hedge by reducing the position size
-        position_size_factor = 1 / (abs(z) - 1.5)
+        #position_size_factor = 1 / (abs(z) - 1.5)
         GlobalVariables.last_signal = "OPEN"
-        return "OPEN", position_size_factor # this is the signal to buy into the swap
+        return "OPEN" #, position_size_factor # this is the signal to buy into the swap
     elif abs(z) < 0.5 and GlobalVariables.last_signal == "OPEN": # can't start with a close!
-        position_size_factor = 1
+        #position_size_factor = 1
         GlobalVariables.last_signal = "CLOSE"
-        return "CLOSE", position_size_factor # this is the signal to close out the swap 
+        return "CLOSE" #, position_size_factor # this is the signal to close out the swap 
     
     return None, None
     # if neither of these is satisfied then we return nothing 
